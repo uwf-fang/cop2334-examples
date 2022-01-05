e1f1f93ec6dd75cf94508d2b822b4b9d5bd88759
Ian Fang <xf19@my.fsu.edu>
2022-01-05 15:51:06 -0600

    organize files

---
+++ ch02-variables/variables.cpp
@@ -0,0 +1,2 @@
+if (age > 1 && age < 10);  // still compile, but the ; here make the if statement runs with empty body
+  cout << "Young child\n"; // this line will always execute because it is not part of the if statement any more
